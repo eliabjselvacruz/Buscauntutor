a521406fbf9dc989acd16b6c3fc0e624b177ae47
FLopez <faviolopez8@gmail.com>
2018-11-15 15:36:10 -0600

    capturando las sesiones de los usuarios

--- js/InicioSesion.js
+++ js/InicioSesion.js
@@ -1,23 +1,14 @@
 //Obteniendo los elementos del formulario de Incio de sesión
-const txtEmail = document.getElementById('usuario');
-const txtClave = document.getElementById('clave_u');
-const btnLogin = document.getElementById('btn_login');
-//const btnRegistro = document.getElementById('btn_registro');
 const btnGoogle = document.getElementById('btn_google');
-const frm_Sesion = document.getElementById('frm_Sesion');
 //Incio de Sesión con Google
 //Registrarse con la cuenta de google.
 btnGoogle.addEventListener('click', () => {
     var provider = new firebase.auth.GoogleAuthProvider();
-   firebase.auth()
-   .signInWithPopup(provider).
+   firebase.auth().signInWithPopup(provider).
    then(function(result) {
-       console.log(result.user);
        guardaDatos(result.user);
-       //$('#btn_google').hide();
-       //$('#btn_login').hide();
-       $('#frm_Sesion').hide();
-       alert("Hola: "+ result.user.displayName+" "+"has iniciado sesión");
+      document.location.href = "vista_PerfilUsuario.html";
+             
    }).catch(function(error) {
     var errorCode = error.code;
     var errorMessage = error.message;
@@ -28,11 +19,7 @@ btnGoogle.addEventListener('click', () => {
     console.log(errorCode);
     console.log(errorMessage);
     console.log(email);
-    });
-
-    //Lamando el metodo par la sesiondel usuario especifico
-    sesionUsuario();
-    
+    });    
 });
 
 //Guardando datos del usuario registrado con cuenta de google
@@ -47,32 +34,8 @@ function guardaDatos(user) {
         firebase.database().ref("clientes/" + user.uid)
         .set(usuario);
     }
-//Manejando las sesiones 
-function sesionUsuario() {
-var user = firebase.auth().currentUser;
 
-if (user != null) {
-  user.providerData.forEach(function (profile) {
-    console.log("Sign-in provider: " + profile.providerId);
-    console.log("  Provider-specific UID: " + profile.uid);
-    console.log("  Name: " + profile.displayName);
-    console.log("  Email: " + profile.email);
-    console.log("  Photo URL: " + profile.photoURL);
-        });
-    }
-}
-
-//Litening state of user
-/*firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is "signed in.""
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      // ...
-    }
-  });*/
+/*var btnGoogle = document.getElementById('btn_google');
+btnGoogle.addEventListener('click', () =>{
+    sesionGoogle();
+})*/
