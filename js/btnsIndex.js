daf3605cd6cc5c7c798b0f5a8c73ff8c9c2a9c4b
FLopez <faviolopez8@gmail.com>
2018-11-18 15:56:22 -0600

    modificación formularios

--- js/btnsIndex.js
+++ js/btnsIndex.js
@@ -5,7 +5,12 @@ document.getElementById('registro_T').addEventListener('click',function () {
     document.location.href = "public/Formularios/vista_RegistroDocente.html";
   });
 
+  //Redireccionando a la vista de inicio de sesion
 document.getElementById('inicio_S').addEventListener('click',function () {
     document.location.href = "public/Formularios/vista_InicioSesion.html";
   });
-  
+
+  //Redireccionando a la pagina principal.
+  document.getElementById('pag_ini').addEventListener('click',function () {
+    document.location.href = "/index.html";
+  });
