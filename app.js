9ee295db6bdb8fcb72261bfdaf8c76932205014b
Snackz <klissman010@gmail.com>
2018-11-18 12:45:54 -0600

    Cambios en index.html Recursos

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ http.listen(5200,function() {
     console.log('server listening on port *5200');  
   });
 
+
+app.use(express.static(__dirname+'/recursos'));
+
 app.get('/', function(req, res){
     res.sendfile(__dirname+'/index.html');
+});
+
+app.get('/fondo4.jpeg',function(req,res) {
+   res.sendfile(__dirname+'/fondo4.jpeg'); 
 });
 
 io.sockets.on('connection',function(socket){
