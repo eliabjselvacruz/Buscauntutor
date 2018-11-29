bc4a569879d3111d911710bfc0b4b9007624534f
Snackz <klissman010@gmail.com>
2018-11-28 18:01:06 -0600

    Agregando aun más redirecciones :,v

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ http.listen(5200,function() {
     console.log('server listening on port *5200');  
   });
 
-
+//Recursos
 app.use(express.static(__dirname+'/recursos'));
 app.use(express.static(__dirname+'/js'));
 // app.use(express.static(__dirname+'/public/Formularios'));
@@ -59,7 +59,19 @@ app.get('/public/Formularios/vista_ReservaPerfilDocente.html',function(req, res)
 
 app.get('/public/Formularios/vista_PerfilDocente.html',function(req, res) {
     res.sendFile(__dirname+'/public/Formularios/vista_PerfilDocente.html');
-})
+});
+
+app.get('/public/Formularios/vista_PerfilDocente.html',function(req,res) {
+   res.sendFile(__dirname+'/public/Formularios/vista_PerfilDocente.html'); 
+});
+
+app.get('/public/Formularios/vista_EdicionPerfilDocente.html',function(req,res) {
+    res.sendFile(__dirname+'/public/Formularios/vista_EdicionPerfilDocente.html');
+});
+
+app.get('/public/Formularios/vista_ActualizacionClaveDocente.html',function(req,res) {
+    res.sendfile(__dirname+'/public/Formularios/vista_ActualizacionClaveDocente.html');
+});
 
 //Message sockets
 io.sockets.on('connection',function(socket){
